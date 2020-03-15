d3f85c1d6547af5b7beac41543c2591d5147ce5a
adrianyuson <adrianyuson@gmail.com>
2020-03-14 22:40:02 -0700

    Updates

---
+++ precache-manifest.967bb1cc172cf0d38063dc06ad826ed4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f24b556395a609bf091f4ed842a700",
+    "url": "/covid19-stats/index.html"
+  },
+  {
+    "revision": "021ce8f359a0dde5bd67",
+    "url": "/covid19-stats/static/css/main.1df3e442.chunk.css"
+  },
+  {
+    "revision": "d2f616dbacd3354aaa35",
+    "url": "/covid19-stats/static/js/2.38afaf71.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/covid19-stats/static/js/2.38afaf71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "021ce8f359a0dde5bd67",
+    "url": "/covid19-stats/static/js/main.84c23580.chunk.js"
+  },
+  {
+    "revision": "d12792d1bce30fc7bbb3",
+    "url": "/covid19-stats/static/js/runtime-main.3978539f.js"
+  }
+]);
